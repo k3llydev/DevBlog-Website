4788c8265521a0d30b642729586e751dc4661555
k3llydev <hello@k3lly.dev>
2023-01-22 00:39:37 -0600

    HotFix: Attempt to fix 404 page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,7 +1,9 @@
-import NotFoundPage from './404';
+import NotFound from './404';
 
-export const getInitialProps = ({ res, err }: any) => ({
-    statusCode: 404
-});
-
-export default NotFoundPage;
+/**
+ * The only purpose of this file is redirecting HTTP 500 Errors to a 404 since there is no server-side processing. 
+ */
+function Error() { return NotFound; }
+Error.getInitialProps = () => ({ statusCode: 404 });
+  
+export default Error;
