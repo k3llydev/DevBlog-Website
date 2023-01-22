2ab5d85509a865b024f0485f29a108a05bddc4af
k3llydev <hello@k3lly.dev>
2023-01-22 02:54:43 -0600

    HotFix: Solved problem of 500 error in SSG by replacing the entire 500 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -7,32 +7,32 @@ import { CONSTANTS } from '../lib/constants';
 
 export const getStaticProps: GetStaticProps<Page.StaticGenericProps> = () => {
   return {
-      props: {
-          header: {
-            show: true,
-            detectBackNavigation: true,
-            variant: 'default'
-          },
-          seo: {
-            title: CONSTANTS.NOT_FOUND.TITLE,
-            description: CONSTANTS.NOT_FOUND.DESCRIPTION,
-            baseName: CONSTANTS.NOT_FOUND.NAME,
-            url: new URL('/404', CONSTANTS.APP_HOST).href,
-            keywords: CONSTANTS.NOT_FOUND.KEYWORDS
-          }
+    props: {
+      header: {
+        show: true,
+        detectBackNavigation: true,
+        variant: 'default'
+      },
+      seo: {
+        title: CONSTANTS.NOT_FOUND.TITLE,
+        description: CONSTANTS.NOT_FOUND.DESCRIPTION,
+        baseName: CONSTANTS.NOT_FOUND.NAME,
+        url: new URL('/404', CONSTANTS.APP_HOST).href,
+        keywords: CONSTANTS.NOT_FOUND.KEYWORDS
       }
+    }
   };
 };
 
 const NotFound: NextPage<Page.StaticGenericProps> = ({ seo }) => {
   return (
-    <div>
+    <>
       <GenericHead seo={seo} />
       <section className={styles.homeCenteredContainer}>
-          <Image className={styles.largeImageBorder} src="/404-image-asset.gif" width={498} height={371} unoptimized alt="Not found" />
-          <h1 className={styles.notFoundLabel}>404 <span className={styles.lightText}>|</span> Not found</h1>
+        <Image className={styles.largeImageBorder} src="/404-image-asset.gif" width={498} height={371} unoptimized alt="Not found" />
+        <h1 className={styles.notFoundLabel}>404 <span className={styles.lightText}>|</span> Not found</h1>
       </section>
-    </div>
+    </>
   )
 }
 
--- pages/_error.tsx
+++
@@ -1,9 +0,0 @@
-import NotFound from './404';
-
-/**
- * The only purpose of this file is redirecting HTTP 500 Errors to a 404 since there is no server-side processing. 
- */
-function Error() { return NotFound; }
-Error.getInitialProps = () => ({ statusCode: 404 });
-  
-export default Error;
